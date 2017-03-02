a9982163985f7b2d595188c61747494d0aeb3e73
Sid <siddharthasadhukhan2009@gmail.com>
2017-03-03 00:47:00 +0530

    Add files via upload

---
+++ char_linked_list.c
@@ -0,0 +1,56 @@
+// Linkedlist using charecter
+// Author: Sid
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+// Defining node
+
+struct node {
+	char data[1];
+	struct node* next;
+};
+
+
+struct node* top=NULL;
+
+push(char x){
+	struct node* temp;
+	temp=(struct node*)malloc(sizeof(struct node));
+	if(top!=NULL){
+		(*temp).data[0]=x;
+		(*temp).next=top;
+		top=temp;
+	}else{
+		(*temp).data[0]=x;
+		(*temp).next=NULL;
+		top=temp;	
+	}
+	stackstatus();
+}
+
+// Print the inserted element in the Linked List
+
+stackstatus(){
+	struct node* temp;
+	temp=top;
+	while(temp!=NULL){
+		printf("%3c  ",(*temp).data[0]);
+		temp=(*temp).next;
+	}
+}
+
+main(){
+	char expression[50];
+	int i;
+	printf("Enter any Expression: ");
+	scanf("%s",&expression);
+	for(i=0;i<strlen(expression);i++){
+		push(expression[i]);
+	}
+	getch();
+}
+
